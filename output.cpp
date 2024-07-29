93ca6ffc70e1a3a2423e5b1de1c52d21e2c6dec7
ShahriarRahman4 <srrifat578@gmail.com>
2024-07-30 00:01:48 +0600

    second phrase

---
+++ output.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+int num1=10;
+int num2=20;
+char value= 'V';
+
+cout<<"1st number is ="<<num1<<endl<<"2nd number is ="<<num2<<endl;
+cout<<"Charecter ="<<value;
+
+return 0;
+
+
+
+
+
+
+}
---
+++ string1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+char name[]="Shahriar Rahman";
+cout<<"My name is : "<<name;
+
+
+    return 0;
+}
